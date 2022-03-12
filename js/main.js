085ee4c2d5ab9ce89e0f1dadad4248ff83d064fb
azuma2 <kafuka1192@gmail.com>
2022-03-12 14:24:21 +0900

    commit22

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-window.addEventListener( 'resize', function() {
-// リサイズ時に行う処理
+
+  
+
+  
   
   
   const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
+  const nav = document.getElementById("nav");
+  nav.classList.toggle('in');
 });
   
-}, false );
