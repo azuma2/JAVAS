fbe7ec04995562641081ec1bde40df13b97d6e3f
azuma2 <kafuka1192@gmail.com>
2022-03-13 16:28:39 +0900

    commit24

--- js/main.js
+++ js/main.js
@@ -10,4 +10,22 @@ target.addEventListener('click', () => {
   const nav = document.getElementById("nav");
   nav.classList.toggle('in');
 });
+
+(function($) {
+$(function() {
+var $header = $(‘.menu2’);
+// Nav Fixed
+$(window).scroll(function() {
+if ($(window).scrollTop() > 350) {
+$header.addClass(‘fixed’);
+} else {
+$header.removeClass(‘fixed’);
+}
+});
+// Nav Toggle Button
+$(‘#nav-toggle, #global-nav ul li a’).click(function(){
+$header.toggleClass(‘open’);
+});
+});
+})(jQuery);
   
